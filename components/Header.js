9b8e27aba073988f38a408d509496a954769aa7e
hadardg <hgoldst1@ucsc.edu>
2020-08-15 19:40:02 -0700

    adding prompts json

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import React, {useRef} from 'react'
 import { View, Text, StyleSheet} from 'react-native'
 
+import promptsJSON from '../prompts.json'
 
 
 
@@ -9,18 +10,21 @@ const Header = props => {
    // const[randNum, setRandNum] = useState(null)
     const randNum = useRef(null)
 
+    let prompt = "prompts" + props.currentJournal.toString()
+    let todaysPrompt = promptsJSON.[prompt][0]
+
     //here we store the possible journal prompts to show the user 
     //TODO: change to JSON and import 
-    const prompts = [
+  /*  const prompts = [
         "What do I love about my life?",
         "What do I feel like my life is missing and how can I get more of what I need?",
         "Where do I want to be in 5 years?",
         "Who are the people in my life that make me the happiest?",
         "heres just a reall long prompt that I need too be so long like so long like so so so so so long just to see if it will all fit when I print it "
     ]
-
+*/
     //generate a random number to pick a random prompt 
-     randNum.current = Math.floor(Math.random() * (prompts.length)) 
+    // randNum.current = Math.floor(Math.random() * (prompts.length)) 
 
     return(
         <View style = {styles.header}>
@@ -29,7 +33,7 @@ const Header = props => {
                 style = {styles.prompt} 
                 adjustsFontSizeToFit ={true} 
                 numberOfLines ={3}>
-                {prompts[randNum.current]}</Text>
+                {todaysPrompt}</Text>
         </View> 
     )
 };
